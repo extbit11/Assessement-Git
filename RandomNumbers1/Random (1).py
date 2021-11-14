21ef2df8b2c213a4f6144fa8b4728fe327a90a29
extbit11 <binkeb11@gmail.com>
2021-11-14 15:16:52 +0100

    first commit

---
+++ RandomNumbers1/Random (1).py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import random
+
+class Random:
+    def __init__(self,n,seeda,seedb):
+        self.amount=n
+        self.minRange=seeda
+        self.maxRange=seedb
+
+    def myfunc(self):
+        if self.maxRange <= 0:
+            print("maxRange should be greater than 0")
+        elif self.maxRange >0 and self.minRange < 0:
+            print("minRange should be greater or equal than 0")
+        elif self.maxRange > 0 and self.minRange >= 0 and self.minRange >= self.maxRange:
+            print("minRange should be lesser than maxRange")
+        elif self.maxRange > 0 and self.minRange >= 0 and self.minRange < self.maxRange:
+
+            numbers = []
+            while len(numbers) != self.amount:
+                a = self.maxRange
+                b = self.maxRange
+                while a == 0 or a == self.maxRange:
+                    a = random.randint(0, self.maxRange)
+                while b < 0 or b == self.maxRange:
+                    b = random.randint(0, self.maxRange)
+
+                rn = (a * self.minRange + b) % self.maxRange
+                if rn >=self.minRange and rn <= self.maxRange :
+                    numbers.append(rn)
+            print(numbers)
+x = Random(5,10,30)
+x.myfunc()
+
+
+# In[ ]:
+
+
+
+
